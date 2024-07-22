d5173bc48bb7d3912394d87a1cccbd7e3e646099
Ankit Kumar <ankit.kumar@privado.ai>
2024-07-22 17:25:44 +0530

    added unit test

---
+++ joern-cli/frontends/csharpsrc2cpg/src/test/scala/io/joern/csharpsrc2cpg/querying/dataflow/InterfaceDataflowTests.scala
@@ -0,0 +1,89 @@
+package io.joern.csharpsrc2cpg.querying.dataflow
+
+import io.joern.csharpsrc2cpg.testfixtures.CSharpCode2CpgFixture
+import io.shiftleft.semanticcpg.language.*
+import io.joern.dataflowengineoss.language.*
+
+class InterfaceDataflowTests extends CSharpCode2CpgFixture(withDataFlow = true) {
+
+  "simple interface dataflow" should {
+    val cpg = code(
+      """
+        |namespace Anything.Any
+        |{
+        |    public class AnotherRepoImplementation : BaseRepo
+        |    {
+        |        private readonly DB _db;
+        |
+        |        public AnotherRepoImplementation(DB db)
+        |        {
+        |            _db = db;
+        |        }
+        |
+        |        public override AnyValue fun(string x)
+        |        {
+        |            return _db.put(x);
+        |        }
+        |    }
+        |}
+        |""".stripMargin,
+      "index.cs"
+    )
+      .moreCode(
+        """
+          |namespace Anything.Any
+          |{
+          |    public interface IRepo
+          |    {
+          |        AnyValue fun(string x);
+          |    }
+          |}
+          |""".stripMargin,
+        "IService.cs"
+      )
+      .moreCode(
+        """
+          |using Anything.Any;
+          |
+          |namespace Anything.ApiEndpoint
+          |{
+          |    public class EndpointClass
+          |    {
+          |        private readonly IRepo _repo;
+          |
+          |        public EndpointClass(IRepo repo)
+          |        {
+          |            _repo = repo;
+          |        }
+          |
+          |        public AnyValue Get()
+          |        {
+          |            var firstName = "firstName";
+          |            var any = _repo.fun(firstName);
+          |            return any;
+          |        }
+          |    }
+          |}
+          |""".stripMargin,
+        "service.cs"
+      )
+      .moreCode(
+        """
+          |namespace Anything.Any
+          |{
+          |    public abstract class BaseRepo : IRepo
+          |    {
+          |        public abstract AnyValue fun(string firstName);
+          |    }
+          |}
+          |""".stripMargin,
+        "BaseRepo.cs"
+      )
+
+    "find path from firstName to fun call" in {
+      val src  = cpg.literal.codeExact("firstName").l
+      val sink = cpg.call.nameExact("put")
+      sink.reachableBy(src).size shouldBe 1
+    }
+  }
+}
